a24cdd913d72b5f07e4c861f842653de044b37d1
Davyd McColl <davydm@gmail.com>
2024-09-27 15:08:27 +0200

    :sparkles: more trait stuff & str vs string & slices

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -4,6 +4,27 @@ pub struct WrappingU32 {
     value: u32,
 }
 
+impl From<i16> for WrappingU32 {
+    fn from(value: i16) -> Self {
+        if value < 0 {
+            panic!("input value must be >= 0")
+        }
+        WrappingU32 {
+            value: u32::from(value)
+        }
+    }
+}
+impl From<i32> for WrappingU32 {
+    fn from(value: i32) -> Self {
+        if value < 0 {
+            panic!("input value must be >= 0")
+        }
+        WrappingU32 {
+            value: u32::from(value)
+        }
+    }
+}
+
 fn example() {
     let wrapping: WrappingU32 = 42.into();
     let wrapping = WrappingU32::from(42);
